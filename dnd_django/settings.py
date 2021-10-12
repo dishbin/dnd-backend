1892fe26f308e260d211f71eb9294b080c936c92
crener <cole.rener@outlook.com>
2021-10-11 23:02:07 -0400

    added cors

--- dnd_django/settings.py
+++ dnd_django/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'dnd',
     'django_extensions',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'dnd_django.urls'
